f51c50911c287c0392fc6e2caee72df3802648bd
William He <wjhe2@wisc.edu>
2025-04-30 04:42:01 -0500

    ugh

--- TCPImplementation/TCPSender.java
+++ TCPImplementation/TCPSender.java
@@ -113,6 +113,8 @@ public class TCPSender {
             /*ack=*/true
         );
         sendPacket(ackPacket);
+
+        baseSequenceNumber = nextSequenceNumber;
     }
 
     private void sendFile() throws IOException {
